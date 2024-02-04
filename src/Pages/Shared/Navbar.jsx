b04331dd53f6c4426932960bfa96c13a7d479247
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-02-04 20:28:56 +0600

    Basic Route & Nested Routing Set up

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Navbar Section</h1>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErroPage from "../Pages/ErroPage";
+import Home from "../Pages/Home/Home";
+
 
 
 const router = createBrowserRouter([
         {
             path: "/",
-            element: <div>Hello world!</div>,
+            element: <MainLayout></MainLayout>,
+            errorElement: <ErroPage></ErroPage>,
+            children: [
+                {
+                    path: "/",
+                    element: <Home></Home>
+                }
+            ]
         },
     ]);
 
