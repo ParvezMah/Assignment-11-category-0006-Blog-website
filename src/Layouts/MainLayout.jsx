95930203ff9c95b3f6f91d84de6eacabfeb13cf2
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-02-05 11:32:17 +0600

    Basic Header & Footer set Up

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar";
 
+
 const MainLayout = () => {
     return (
         <div>
